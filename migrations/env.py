84d460226bada464bf986664c09153e18ed9d870
solenfw <nhuyhoang1012005@gmail.com>
2026-06-11 03:46:17 +0700

    chore: generate initial Alembic migration and transition to code-first

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,18 @@
+import os, sys
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, root_dir)
+sys.path.insert(0, os.path.join(root_dir, "server"))
+sys.path.insert(0, os.path.join(root_dir, "server", "app"))
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from server.app.db.database import Base
+from server.app.db import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
